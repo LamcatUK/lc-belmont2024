cd00441839c0bbaaedb2b33b3b96c76ce2d0f049
ChillibyteUK <git@lamcat.co.uk>
2025-02-02 19:05:14 +0000

    Update lc-theme.php

--- inc/lc-theme.php
+++ inc/lc-theme.php
@@ -10,23 +10,23 @@ require_once LC_THEME_DIR . '/inc/lc-blog.php';
 if (function_exists('acf_add_options_page')) {
     acf_add_options_page(
         array(
-            'page_title' 	=> 'Site-Wide Settings',
-            'menu_title'	=> 'Site-Wide Settings',
-            'menu_slug' 	=> 'theme-general-settings',
-            'capability'	=> 'edit_posts',
+            'page_title'     => 'Site-Wide Settings',
+            'menu_title'    => 'Site-Wide Settings',
+            'menu_slug'     => 'theme-general-settings',
+            'capability'    => 'edit_posts',
         )
     );
 }
 
 function widgets_init()
 {
-  
+
     register_nav_menus(array(
         'primary_nav' => 'Primary Nav',
         'footer_menu_1' => 'Footer 1',
         'footer_menu_2' => 'Footer 2',
     ));
- 
+
     unregister_sidebar('hero');
     unregister_sidebar('herocanvas');
     unregister_sidebar('statichero');
@@ -34,7 +34,7 @@ function widgets_init()
     unregister_sidebar('right-sidebar');
     unregister_sidebar('footerfull');
     unregister_nav_menu('primary');
- 
+
     add_theme_support('disable-custom-colors');
     add_theme_support(
         'editor-color-palette',
@@ -100,19 +100,19 @@ function register_lc_dashboard_widget()
 
 function lc_dashboard_widget_display()
 {
-    ?>
-<div style="display: flex; align-items: center; justify-content: space-around;">
-    <img style="width: 50%;"
-        src="<?= get_stylesheet_directory_uri().'/img/lc-full.jpg'; ?>">
-    <a class="button button-primary" target="_blank" rel="noopener nofollow noreferrer"
-        href="mailto:hello@lamcat.co.uk/">Contact</a>
-</div>
-<div>
-    <p><strong>Thanks for choosing Lamcat!</strong></p>
-    <hr>
-    <p>Got a problem with your site, or want to make some changes & need us to take a look for you?</p>
-    <p>Use the link above to get in touch and we'll get back to you ASAP.</p>
-</div>
+?>
+    <div style="display: flex; align-items: center; justify-content: space-around;">
+        <img style="width: 50%;"
+            src="<?= get_stylesheet_directory_uri() . '/img/lc-full.jpg'; ?>">
+        <a class="button button-primary" target="_blank" rel="noopener nofollow noreferrer"
+            href="mailto:hello@lamcat.co.uk/">Contact</a>
+    </div>
+    <div>
+        <p><strong>Thanks for choosing Lamcat!</strong></p>
+        <hr>
+        <p>Got a problem with your site, or want to make some changes & need us to take a look for you?</p>
+        <p>Use the link above to get in touch and we'll get back to you ASAP.</p>
+    </div>
 <?php
 }
 
@@ -129,7 +129,7 @@ function lc_theme_enqueue()
 
     $css_version = $theme_version . '.' . filemtime(get_stylesheet_directory() . $theme_styles);
 
-    wp_deregister_script('jquery');
+    // wp_deregister_script('jquery');
 
     wp_enqueue_style('swiper-style', "https://unpkg.com/swiper/swiper-bundle.min.css", array());
     wp_enqueue_script('swiper', "https://unpkg.com/swiper/swiper-bundle.min.js", array(), null, true);
@@ -140,9 +140,8 @@ function lc_theme_enqueue()
     // wp_enqueue_script('timelyScript', 'https://book.gettimely.com/widget/book-button-v1.5.js', array(), null, true);
 
     $js_version = $theme_version . '.' . filemtime(get_stylesheet_directory() . $theme_scripts);
-    
-    wp_enqueue_script('child-understrap-scripts', get_stylesheet_directory_uri() . $theme_scripts, array(), $js_version, true);
 
+    wp_enqueue_script('child-understrap-scripts', get_stylesheet_directory_uri() . $theme_scripts, array(), $js_version, true);
 }
 add_action('wp_enqueue_scripts', 'lc_theme_enqueue');
 
@@ -162,15 +161,16 @@ add_filter('wpcf7_autop_or_not', '__return_false');
 
 add_shortcode('timely_button', 'timelyButton');
 
-function timelyButton() {
+function timelyButton()
+{
     ob_start();
-    ?>
+?>
     <script id="timelyScript" src="https://book.gettimely.com/widget/book-button-v1.5.js"></script>
-<script>
-    new timelyButton("belmontskinandlaserclinic", {
-        "style": "dark"
-    });
-</script>
+    <script>
+        new timelyButton("belmontskinandlaserclinic", {
+            "style": "dark"
+        });
+    </script>
 <?php
     return ob_get_clean();
 }
@@ -178,7 +178,8 @@ function timelyButton() {
 
 //-------------- pricing schema
 
-function extract_pricing_table_data($block_data) {
+function extract_pricing_table_data($block_data)
+{
     $offers = [];
     $product_count = $block_data['products'];
 
@@ -200,14 +201,14 @@ function extract_pricing_table_data($block_data) {
                 "availability" => "https://schema.org/InStock",
                 "name" => "{$product_title} - {$description}"
             ];
-
         }
     }
 
     return $offers;
 }
 
-function get_pricing_table_data_from_blocks($blocks) {
+function get_pricing_table_data_from_blocks($blocks)
+{
     $all_offers = [];
 
     foreach ($blocks as $block) {
@@ -226,7 +227,8 @@ function get_pricing_table_data_from_blocks($blocks) {
 }
 
 // Function to generate the schema
-function generate_pricing_table_schema() {
+function generate_pricing_table_schema()
+{
     // Get all blocks on the page
     $blocks = parse_blocks(get_the_content());
     $offers = get_pricing_table_data_from_blocks($blocks);
@@ -275,10 +277,10 @@ function generate_pricing_table_schema() {
                     "closes" => "17:00"
                 ],
                 [
-                "@type" => "OpeningHoursSpecification",
-                "dayOfWeek" => "Saturday",
-                "opens" => "09:00",
-                "closes" => "15:00"
+                    "@type" => "OpeningHoursSpecification",
+                    "dayOfWeek" => "Saturday",
+                    "opens" => "09:00",
+                    "closes" => "15:00"
                 ]
             ],
             "sameAs" => [
@@ -294,7 +296,8 @@ function generate_pricing_table_schema() {
 
 
 //--------------- Generate Breadcrumb schema
-function generate_breadcrumb_schema() {
+function generate_breadcrumb_schema()
+{
     $schema = [
         "@context" => "https://schema.org",
         "@type" => "BreadcrumbList",
