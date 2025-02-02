0aaa5e04d8dc79485d52ae2111552eae810f3ecc
ChillibyteUK <git@lamcat.co.uk>
2025-02-02 20:50:55 +0000

    Update qr.php

--- page-templates/qr.php
+++ page-templates/qr.php
@@ -7,16 +7,16 @@ defined('ABSPATH') || exit;
 
 get_header('connect');
 ?>
-<main>
+<main class="mt-3">
     <img src="<?= get_stylesheet_directory_uri() ?>/img/belmont-logo.png" alt="Belmont Skin & Laser Clinic">
     <div class="container-xl pt-5 qr">
-        <a class="qr__button" href="mailto:<?= get_field('contact_email', 'options') ?>">
+        <a class="button button-outline-primary d-block text-center" href="mailto:<?= get_field('contact_email', 'options') ?>">
             <i class="fa-solid fa-envelope"></i> Email Us
         </a>
-        <a class="qr__button" href="tel:<?= parse_phone(get_field('contact_phone', 'options')) ?>">
+        <a class="button button-outline-primary d-block text-center" href="tel:<?= parse_phone(get_field('contact_phone', 'options')) ?>">
             <i class="fa-solid fa-envelope"></i> Call <?= get_field('contact_phone', 'options') ?>
         </a>
-        <a class="qr__button" href="/book-now/">
+        <a class="button button-outline-primary d-block text-center" href="/book-now/">
             <i class="fa-solid fa-envelope"></i> Book Now
         </a>
 
@@ -27,21 +27,21 @@ get_header('connect');
 
         if ($s['linkedin_url'] ?? null) {
         ?>
-            <a href="<?= $s['linkedin_url'] ?>" target="_blank" class="qr__button">
+            <a href="<?= $s['linkedin_url'] ?>" target="_blank" class="button button-outline-primary d-block text-center">
                 <i class="fa-brands fa-linkedin-in"></i> LinkedIn
             </a>
         <?php
         }
         if ($s['instagram_url'] ?? null) {
         ?>
-            <a href="<?= $s['instagram_url'] ?>" target="_blank" class="qr__button">
+            <a href="<?= $s['instagram_url'] ?>" target="_blank" class="button button-outline-primary d-block text-center">
                 <i class="fa-brands fa-instagram"></i> Instagram
             </a>
         <?php
         }
         if ($s['facebook_url'] ?? null) {
         ?>
-            <a href="<?= $s['facebook_url'] ?>" target="_blank" class="qr__button">
+            <a href="<?= $s['facebook_url'] ?>" target="_blank" class="button button-outline-primary d-block text-center">
                 <i class="fa-brands fa-facebook-f"></i> Facebook
             </a>
         <?php
